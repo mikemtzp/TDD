cd8d3dc1ce19e7315f32abe1addddb460d8d44c9
mikemtzp <mikemp8226@gmail.com>
2022-09-01 10:36:26 -0500

    Add raise error test in factorial

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -8,4 +8,8 @@ describe 'Factorial method' do
   it 'returns 1 as special case that the factorial of 0 is 1' do
     expect(@solver.factorial(0)).to be(1)
   end
+
+  it 'raises error if number passed is negative' do
+    expect { @solver.factorial(-1) }.to raise_error('Please introduce a positive number')
+  end
 end
