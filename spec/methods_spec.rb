dcafd57b69bf4807071cf5aeb96ed2ee27788b17
mikemtzp <mikemp8226@gmail.com>
2022-09-01 11:12:17 -0500

    Add factorial results tests

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -6,10 +6,18 @@ describe 'Factorial method' do
   end
 
   it 'returns 1 as special case that the factorial of 0 is 1' do
-    expect(@solver.factorial(0)).to be(1)
+    expect(@solver.factorial(0)).to eq(1)
   end
 
   it 'raises error if number passed is negative' do
     expect { @solver.factorial(-1) }.to raise_error('Please introduce a positive number')
+  end
+
+  it 'returns 120 as the factorial of 5' do
+    expect(@solver.factorial(5)).to eq(120)
+  end
+
+  it 'returns 1,307,674,368,000 as the factorial of 15' do
+    expect(@solver.factorial(15)).to eq(1_307_674_368_000)
   end
 end
